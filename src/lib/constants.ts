7169805744d89086c7a978e097c1ab404e67b017
Sayyam <dev.sayyam@gmail.com>
2025-06-14 16:25:50 +0530

    fix: update contact links in footer section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,6 @@
 export const WA_LINK =
   'https://api.whatsapp.com/send/?phone=919028976883&text=Hi! I’m interested in your interior design services. I’d love to discuss how we can bring my vision to life. Looking forward to hearing from you soon!'
+export const INSTA_LINK =
+  'https://www.instagram.com/cubism_theinteriordesignstudio/reels/'
+export const EMAIL_LINK =
+  'mailto:cubisminteriors@gmail.com?subject=Interior%20Design%20Inquiry&body=Hi!%20I%E2%80%99m%20interested%20in%20your%20interior%20design%20services.%20I%E2%80%99d%20love%20to%20discuss%20how%20we%20can%20bring%20my%20vision%20to%20life.%20Looking%20forward%20to%20hearing%20from%20you%20soon!'
